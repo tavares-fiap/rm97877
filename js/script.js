f9d988f5bc07f0621c7a206eb0f3de642029c82d
tavares-fiap <RM97877@fiap.com.br>
2023-04-25 14:55:52 -0300

    copia breq na branch

--- js/script.js
+++ js/script.js
@@ -30,3 +30,27 @@ seletores.forEach((item)=>{
 //Math.Ceil
 //Math.Random
 //Math.Floor
+let tmp = ""
+
+function mudarCor() {
+    
+    let r = 0;
+    let g = 0;
+    let b = 0;
+
+    r = Math.round(Math.random() * 255);
+    g = Math.round(Math.random() * 255);
+    b = Math.round(Math.random() * 255);
+    
+    const cabecalho = document.querySelector(".cabecalho");
+    cabecalho.style.backgroundColor= `rgb(${r},${g},${b})`;
+    
+    tmp = setTimeout(mudarCor, 1000);
+
+}
+
+mudarCor();
+
+function mudarImagem(){
+    
+}
