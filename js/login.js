8827d64abcc56e9345221abe8cf02098ec3183ac
rm97877 <rm97877@fiap.com.br>
2023-05-22 09:05:01 -0300

    Cria obj + elemento msg no login/ valid formulario

--- js/login.js
+++ js/login.js
@@ -1,25 +1,34 @@
 "use strict";
 
-let usuario = "";
-let senha = "";
-
-const botaoSubmit = document.querySelector("#btnSubmit");
-
-botaoSubmit.addEventListener("click",()=>{
-
+addEventListener("click", (evt)=>{
+    
     const inputUser = document.querySelector("input[name='nmUser']");
     const inputPass = document.querySelector("input[name='pass']");
-
+    
     const labelUser = document.querySelector("label[for='idUser']");
     const labelPass = document.querySelector("label[for='idPass']");
 
-    if(inputUser.value.length > 3 && inputPass.value.length > 3){
-        // inputUser.setAttribute("style", "outline-color:#00ff00;")
-        // inputPass.setAttribute("style", "outline-color:#00ff00;")
-        labelUser.setAttribute("style","color:#00ff00;");
-        labelPass.setAttribute("style","color:#00ff00;");
-    }else{
-    labelUser.setAttribute("style","color:#ff0000;");
-    labelPass.setAttribute("style","color:#ff0000;");
+    const usuarioLogado = {
+        nomeUsuarioLogado: inputUser.value,
+        senhaUsuarioLogado: inputPass.value
+    }
+
+    if(evt.target.id == "btnSubmit"){
+        
+        if(inputUser.value.length > 3 && inputPass.value.length > 3){
+            // inputUser.setAttribute("style", "outline-color:#00ff00;")
+            // inputPass.setAttribute("style", "outline-color:#00ff00;")
+            labelUser.setAttribute("style","color:#00ff00;");
+            labelPass.setAttribute("style","color:#00ff00;");
+                if(usuarioLogado.nomeUsuarioLogado == "tutu" && usuarioLogado.senhaUsuarioLogado == "123456"){
+                    alert("VALIDADO!");
+                }else{
+                    alert("NÃO VLIDADO");
+                }
+        }else{
+            labelUser.setAttribute("style","color:#ff0000;");
+            labelPass.setAttribute("style","color:#ff0000;");
+        }
+        
     }
 });
